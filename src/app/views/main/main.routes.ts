8fc75d24faea6cc494a16ba66ffdba1412b6eb2e
Alexander Vestergaard Eriksen <alexanderv.eriksen@gmail.com>
2024-08-06 20:19:02 +0200

    Add main and test components

---
+++ src/app/views/main/main.routes.ts
@@ -0,0 +1,14 @@
+﻿import {Routes} from "@angular/router";
+import {TestComponent} from "./components/test/test.component";
+
+export const mainRoutes: Routes = [
+    {
+        path: "",
+        redirectTo: "test",
+        pathMatch: "full"
+    },
+    {
+        path: "test",
+        component: TestComponent
+    }
+]
