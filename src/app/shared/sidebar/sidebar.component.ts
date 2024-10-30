8a1a6c9d3d152906a2a78182127f73f8ab9adc2e
Alexander Vestergaard Eriksen <alexanderv.eriksen@gmail.com>
2024-10-30 11:49:53 +0100

    Add collapsed state memory

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, output } from "@angular/core";
+import { Component, HostListener, OnInit, output } from "@angular/core";
 import { TranslateModule } from "@ngx-translate/core";
 import { INavigationTab } from "../interfaces/navigation-tab.interface";
 import { MatSidenavModule } from "@angular/material/sidenav";
@@ -8,6 +8,7 @@ import { SIDEBAR_SIDEBAR_CONFIG } from "./sidebar-navigations.constants";
 import { NgClass, NgOptimizedImage } from "@angular/common";
 import { SidebarItemComponent } from "./components/sidebar-item/sidebar-item.component";
 import { ISideNavToggle } from "./side-nav-toggle.interface";
+import { LOCAL_STORAGE_KEYS } from "../constants/local-storage.constants";
 
 @Component({
   selector: 'ps-sidebar',
@@ -26,15 +27,21 @@ import { ISideNavToggle } from "./side-nav-toggle.interface";
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
     screenWidth: number = window.innerWidth;
     navigations: INavigationTab[] = SIDEBAR_SIDEBAR_CONFIG;
     collapsed = true;
     toggleSideNav = output<ISideNavToggle>();
 
+    ngOnInit() {
+        this.collapsed = localStorage.getItem(LOCAL_STORAGE_KEYS.SideNavState) as unknown as boolean | true;
+        this.toggleSideNav.emit({screenWidth: this.screenWidth, collapsed: this.collapsed});
+    }
+
     toggleCollapse() {
         this.collapsed = !this.collapsed;
         this.toggleSideNav.emit({screenWidth: this.screenWidth, collapsed: this.collapsed});
+        localStorage.setItem(LOCAL_STORAGE_KEYS.SideNavState, this.collapsed as unknown as string);
     }
 
     @HostListener("window:resize", ["$event"])
