55390e5255a528466e8b76494d45b6f1f106fea5
NicklasFjeldsted <nicklasfjeldstedosbeck@gmail.com>
2024-11-03 11:34:33 +0100

    Added partial translate

---
+++ src/app/core/pipes/partial-translate.pipe.ts
@@ -0,0 +1,26 @@
+import { TranslateService } from "@ngx-translate/core";
+import { inject, Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+    name: "partialTranslate",
+    standalone: true,
+    pure: false,
+})
+export class PartialTranslatePipe implements PipeTransform {
+    translateService = inject(TranslateService);
+
+    transform(value: string, translateKey: string): string;
+    transform(value: string, translateKey: string[]): string;
+
+    transform(value: string, translateKey: string[] | string): string {
+        let newValue = value;
+        if (Array.isArray(translateKey)) {
+            translateKey.forEach((key) => {
+                newValue = newValue.replace(key, this.translateService.instant(key));
+            });
+        } else {
+            newValue = newValue.replace(translateKey, this.translateService.instant(translateKey));
+        }
+        return newValue;
+    }
+}
