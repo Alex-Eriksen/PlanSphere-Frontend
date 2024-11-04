f9516e0b7d9bff34186544f198506b6ce63be43f
NicklasFjeldsted <nicklasfjeldstedosbeck@gmail.com>
2024-11-04 11:58:20 +0100

    Initial commit

--- src/app/views/main/components/frontpage/frontpage.component.ts
+++ src/app/views/main/components/frontpage/frontpage.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { UploadFileDropZoneComponent } from "../../../../shared/upload-logo/upload-logo.component";
+import { InputComponent } from "../../../../shared/input/input.component";
 
 @Component({
   selector: 'ps-frontpage',
   standalone: true,
-  imports: [],
+    imports: [
+        UploadFileDropZoneComponent,
+        InputComponent
+    ],
   templateUrl: './frontpage.component.html',
   styleUrl: './frontpage.component.scss'
 })
