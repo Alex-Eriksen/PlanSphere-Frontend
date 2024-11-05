816269576aba3c7771a1695b85f3c4c5013bf756
Alexander Vestergaard Eriksen <alexanderv.eriksen@gmail.com>
2024-11-05 17:07:20 +0100

    Styles changes

--- src/app/shared/table-headers/job-title.header.ts
+++ src/app/shared/table-headers/job-title.header.ts
@@ -3,17 +3,17 @@ import { TableHeaderType } from "../enums/table-header-type.enum";
 
 export const jobTitleHeaders: ITableHeader[] = [
     {
-        key: "name",
-        label: "JOB_TITLE.NAME_OF",
-        isSortable: true,
-        type: TableHeaderType.Text,
-        maxTextLength: 70,
-    },
-    {
         key: "isInheritanceActive",
         label: "JOB_TITLE.ACTIVE",
         isSortable: true,
         type: TableHeaderType.Toggle,
+        maxTextLength: 70,
+    },
+    {
+        key: "name",
+        label: "JOB_TITLE.NAME_OF",
+        isSortable: true,
+        type: TableHeaderType.Text,
         maxTextLength: 70,
     },
     {
