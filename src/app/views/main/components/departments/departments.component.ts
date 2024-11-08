75ac0e75ed028558f8008d00d77592201d7f0ab2
kasp643d <kas10@hotmail.dk>
2024-11-08 18:01:39 +0100

    fixing userdepartmentlist page issues

--- src/app/views/main/components/departments/departments.component.ts
+++ src/app/views/main/components/departments/departments.component.ts
@@ -1,11 +1,13 @@
 import { Component  } from "@angular/core";
 import { DepartmentListComponent } from "../../../../shared/list-department/department-list.component";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
     selector: 'ps-departments',
     standalone: true,
     imports: [
-        DepartmentListComponent
+        DepartmentListComponent,
+        RouterOutlet
     ],
   templateUrl: './departments.component.html',
   styleUrl: './departments.component.scss'
