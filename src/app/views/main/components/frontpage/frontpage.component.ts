2051cf089f5d6d284ad273fa13d082b7f1aff7e2
NicklasFjeldsted <nicklasfjeldstedosbeck@gmail.com>
2024-11-10 21:59:40 +0100

    Added calendar to front page

--- src/app/views/main/components/frontpage/frontpage.component.ts
+++ src/app/views/main/components/frontpage/frontpage.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from "@angular/core";
+import { ButtonComponent } from "../../../../shared/button/button.component";
+import { LoadingOverlayComponent } from "../../../../shared/loading-overlay/loading-overlay.component";
+import { NgClass, NgIf } from "@angular/common";
+import {
+    generateFormGroup,
+} from "./calendar.utilities";
+import { NonNullableFormBuilder } from "@angular/forms";
+import { LineComponent } from "../../../../shared/line/line.component";
+import { SubHeaderComponent } from "../../../../shared/sub-header/sub-header.component";
+import { SmallHeaderComponent } from "../../../../shared/small-header/small-header.component";
+import { TranslateModule } from "@ngx-translate/core";
+import { CalenderTableComponent } from "../../../../shared/calender-table/calender-table.component";
+import { CalendarSidePanelComponent } from "../../../../shared/calendar-side-panel/calendar-side-panel.component";
+import { CalendarComponent } from "../../../../shared/calendar/calendar.component";
 
 @Component({
   selector: 'ps-frontpage',
   standalone: true,
-    imports: [],
+    imports: [
+        ButtonComponent,
+        LoadingOverlayComponent,
+        NgClass,
+        NgIf,
+        LineComponent,
+        SubHeaderComponent,
+        SmallHeaderComponent,
+        TranslateModule,
+        CalenderTableComponent,
+        CalendarSidePanelComponent,
+        CalendarComponent
+    ],
   templateUrl: './frontpage.component.html',
   styleUrl: './frontpage.component.scss'
 })
-export class FrontpageComponent {
+export class FrontpageComponent implements OnInit {
+    readonly #fb = inject(NonNullableFormBuilder);
+    isPageLoading: boolean = false;
+    formGroup: any;
 
+    ngOnInit() {
+        this.isPageLoading = true;
+        if (this.isPageLoading) {
+            this.formGroup = generateFormGroup(this.#fb);
+            this.isPageLoading = false;
+        }
+    }
 }
