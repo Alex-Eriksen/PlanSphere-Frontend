60b541b077956f186a735b7ca2664a0fe2124d9b
Alexander Vestergaard Eriksen <alexanderv.eriksen@gmail.com>
2024-11-11 17:14:33 +0100

    Added organisation settings page

---
+++ src/app/core/features/organisations/models/organisation.model.ts
@@ -0,0 +1,12 @@
+import { IAddress } from "../../address/models/address.model";
+import { IBase } from "../../../abstract/models/base.model";
+import { IOrganisationSettings } from "./organisation-settings.model";
+
+export interface IOrganisationDetails extends IBase
+{
+    name: string;
+    logoUrl?: string;
+    address: IAddress;
+    settings: IOrganisationSettings;
+    createdAt: Date;
+}
