d43d0b93f6f31f7b504e2baeb06f5f42cdb63b4c
Alexander Vestergaard Eriksen <alexanderv.eriksen@gmail.com>
2024-11-12 14:46:21 +0100

    Fix loading

--- src/app/views/main/components/organisation/components/roles/roles.component.ts
+++ src/app/views/main/components/organisation/components/roles/roles.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from "@angular/core";
 import { SourceLevel } from "../../../../../../core/enums/source-level.enum";
 import { RolesViewComponent } from "../../../../../../shared/roles-view/roles-view.component";
+import { AuthenticationService } from "../../../../../../core/features/authentication/services/authentication.service";
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
 
 @Component({
   selector: 'ps-roles',
@@ -11,7 +13,17 @@ import { RolesViewComponent } from "../../../../../../shared/roles-view/roles-vi
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.scss'
 })
-export class RolesComponent {
+export class RolesComponent implements OnInit {
+    readonly #authService = inject(AuthenticationService);
+    readonly #destroyRef = inject(DestroyRef);
+
     sourceLevel = SourceLevel.Organisation;
-    sourceLevelId = 1;
+    sourceLevelId!: number;
+
+    ngOnInit() {
+        this.#authService.LoggedInUserObservable.pipe(takeUntilDestroyed(this.#destroyRef)).subscribe(loggedInUser => {
+            if (!loggedInUser) return;
+            this.sourceLevelId = loggedInUser.organisationId;
+        })
+    }
 }
