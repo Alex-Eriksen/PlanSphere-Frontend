b1e359b7795350cfb6ee2cdc239a6fb22aec2064
NicklasFjeldsted <nicklasfjeldstedosbeck@gmail.com>
2024-11-17 11:00:08 +0100

    Resolve comments

--- src/app/core/features/workTimes/models/work-time.models.ts
+++ src/app/core/features/workTimes/models/work-time.models.ts
@@ -1,8 +1,8 @@
 import { WorkTimeType } from "./work-time-type.interface";
 import { ShiftLocation } from "../../../enums/shift-location.enum";
+import { IBase } from "../../../abstract/models/base.model";
 
-export interface IWorkTime {
-    id: number;
+export interface IWorkTime extends IBase {
     startDateTime: Date;
     endDateTime: Date | null;
     workTimeType: WorkTimeType;
